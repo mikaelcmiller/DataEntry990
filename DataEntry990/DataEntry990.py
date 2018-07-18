904b0fd260f3416e6cea31879ceacb85eb4b20ef
MikaelCMiller <mikael.miller@erieri.com>
2018-07-18 11:21:02 -0700

    Multiple Page Navigation

--- DataEntry990/DataEntry990.py
+++ DataEntry990/DataEntry990.py
@@ -14,10 +14,13 @@ class DE990App(tk.Tk):
 		container.grid_columnconfigure(0, weight=1)
 	
 		self.frames = {}
-	
-		frame = StartPage(container, self)
-		self.frames[StartPage] = frame
-		frame.grid(row=0, column=0, sticky="nsew")
+		
+		#Page List - use classes to create distinct pages
+		for F in (StartPage, PageOne, PageTwo):
+			frame = F(container, self)
+			self.frames[F] = frame
+			frame.grid(row=0, column=0, sticky="nsew")
+		
 		self.show_frame(StartPage)
 
 	def show_frame(self, cont):
@@ -34,7 +37,34 @@ class StartPage(tk.Frame):
 		label.pack(pady=10, padx=10)
 		
 		button1 = tk.Button(self, text="Visit Page 1", 
-			command=lambda: qf("see this worked"))
+			command=lambda: controller.show_frame(PageOne))
+		button1.pack()
+		button2 = tk.Button(self, text="Visit Page 2", 
+			command=lambda: controller.show_frame(PageTwo))
+		button2.pack()
+
+class PageOne(tk.Frame):
+	def __init__(self, parent, controller):
+		tk.Frame.__init__(self, parent)
+		label = tk.Label(self, text="Page One", font=LARGE_FONT)
+		label.pack(pady=10, padx=10)
+		buttonhome = tk.Button(self, text="Back to Home", 
+			command=lambda: controller.show_frame(StartPage))
+		buttonhome.pack()
+		button2 = tk.Button(self, text="Visit Page 2", 
+			command=lambda: controller.show_frame(PageTwo))
+		button2.pack()
+
+class PageTwo(tk.Frame):
+	def __init__(self, parent, controller):
+		tk.Frame.__init__(self, parent)
+		label = tk.Label(self, text="Page Two!", font=LARGE_FONT)
+		label.pack(pady=10, padx=10)
+		buttonhome = tk.Button(self, text="Back to Home", 
+			command=lambda: controller.show_frame(StartPage))
+		buttonhome.pack()
+		button1 = tk.Button(self, text="Visit Page 1", 
+			command=lambda: controller.show_frame(PageOne))
 		button1.pack()
 
 app = DE990App()
