02c050205fc1cf9e081ea42be2f2a4b794b58af5
MikaelCMiller <mikael.miller@erieri.com>
2018-07-27 16:08:47 -0700

    EOW

--- DataEntry990/DataEntry990.py
+++ DataEntry990/DataEntry990.py
@@ -20,7 +20,7 @@ class DE990App(tk.Tk):
 		self.frames = {}
 		
 		#Page List - use classes to create distinct pages
-		for F in (StartPage, PageOne, PageTwo):
+		for F in (StartPage, PageOne, PageTwo, PFEntry):
 			frame = F(container, self)
 			self.frames[F] = frame
 			frame.grid(row=0, column=0, sticky="nsew")
@@ -46,6 +46,9 @@ class StartPage(tk.Frame):
 		button2 = ttk.Button(self, text="Visit Page 2", 
 			command=lambda: controller.show_frame(PageTwo))
 		button2.pack()
+		buttonPF = ttk.Button(self, text="PF Entry", 
+			command=lambda: controller.show_frame(PFEntry))
+		buttonPF.pack()
 
 class PageOne(tk.Frame):
 	thistext='1. Mikael'
@@ -64,6 +67,9 @@ class PageOne(tk.Frame):
 		button2.pack()
 		buttonprint = ttk.Button(self, text="Print 'Mikael'", command=lambda: self.ttext(self.thistext))
 		buttonprint.pack()
+		buttonPF = ttk.Button(self, text="PF Entry", 
+			command=lambda: controller.show_frame(PFEntry))
+		buttonPF.pack()
 
 #def ptext(param):
 #	print(param)
@@ -86,6 +92,24 @@ class PageTwo(tk.Frame):
 		button1.pack()
 		buttonprint2 = ttk.Button(self, text="Print 'Miller'", command=lambda: self.ttext2(self.thistext))
 		buttonprint2.pack()
+		buttonPF = ttk.Button(self, text="PF Entry", 
+			command=lambda: controller.show_frame(PFEntry))
+		buttonPF.pack()
+
+class PFEntry(tk.Frame):
+	def __init__(self, parent, controller):
+		tk.Frame.__init__(self, parent)
+		label = tk.Label(self, text="PF Entry", font=LARGE_FONT)
+		label.pack(pady=10, padx=10)
+		buttonhome = ttk.Button(self, text="Back to Home", 
+			command=lambda: controller.show_frame(StartPage))
+		buttonhome.pack()
+		button1 = ttk.Button(self, text="Visit Page 1", 
+			command=lambda: controller.show_frame(PageOne))
+		button1.pack()
+		button2 = ttk.Button(self, text="Visit Page 2", 
+			command=lambda: controller.show_frame(PageTwo))
+		button2.pack()
 
 
 app = DE990App()
